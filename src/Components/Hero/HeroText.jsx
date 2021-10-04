578e24b58da8d67bdfde1987c654b33d76d8c063
Mantas <mantaskrincius@gmail.com>
2021-10-04 11:34:56 +0300

    cmoonnnn!

--- src/Components/Hero/HeroText.jsx
+++ src/Components/Hero/HeroText.jsx
@@ -4,7 +4,7 @@ import './HeroText.css'
 export default function HeroText() {
     return (
         <div className="fixing">
-            <h1>Seeking new opportunity</h1>
+            <h1>Seeking new opportunities</h1>
             <p className="subtitle">Welcome to my CV!</p>
         </div>
     )
